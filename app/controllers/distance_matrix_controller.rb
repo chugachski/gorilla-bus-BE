7a11b93bf9131decab5028bfa79e2fc7a3c63014
Yilin Peng <elenayyyp@gmail.com>
2016-08-31 07:29:18 -0400

    env keys

--- app/controllers/distance_matrix_controller.rb
+++ app/controllers/distance_matrix_controller.rb
@@ -4,9 +4,10 @@ class DistanceMatrixController < ApplicationController
     lng = params[:lng]
     stop_lat = params[:stopLat]
     stop_lng = params[:stopLng]
-
-
-    full_query = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=#{lat},#{lng}&destinations=#{stop_lat},#{stop_lng}&key=AIzaSyD5ZfvxOIfkef7YwfrAOWxniSur8U9Lwzo"
+    matrix_key = ENV["DISTANCE_MATRIX_KEY"]
+    puts ENV["DISTANCE_MATRIX_KEY"]
+    
+    full_query = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=#{lat},#{lng}&destinations=#{stop_lat},#{stop_lng}&key=#{matrix_key}"
 
     data = HTTParty.get(full_query)
     render json: data
